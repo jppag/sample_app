ff2579e0b4e7e8101b5d52adbcb5fcc9fb194e6f
Joao Pagnoncelli <jppagnoncelli@gmail.com>
2014-01-09 12:28:32 -0200

    Add Pins and functionality

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140108223327) do
+ActiveRecord::Schema.define(version: 20140109132641) do
+
+  create_table "pins", force: true do |t|
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
